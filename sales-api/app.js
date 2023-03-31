4345e8cfca88bf95e522dedf658172cf6d18dbb6
Menezo <joaovitormenezesm12@gmail.com>
2023-03-31 12:03:40 -0300

    Creating Sales-API Sender

--- sales-api/app.js
+++ sales-api/app.js
@@ -6,6 +6,8 @@ import { connectRabbitMq } from './src/config/rabbitmq/rabbitConfig.js';
 
 import CheckToken from './src/config/auth/CheckToken.js';
 
+import { sendMessageToProductStockUpdateQueue } from './src/modules/product/rabbitmq/productStockUpdateSender.js';
+
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
@@ -14,7 +16,31 @@ connectMongoDb();
 createInitialData();
 connectRabbitMq();
 
-app.use(CheckToken);
+//app.use(CheckToken);
+
+app.get("/teste", (req, res) => {
+    try {
+        sendMessageToProductStockUpdateQueue([
+            {
+                productId: 100,
+                quantity: 2,
+            },
+            {
+                productId: 200,
+                quantity: 3,
+            },
+            {
+                productId: 300,
+                quantity: 5,
+            },
+        ]);
+        return res.status(200).json({ status: 200 });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: true });
+    }
+});
 
 app.get('/api/status',  async (req, res) => {
     
